b386709f42c2fe7f96d37689460c859ab0927108
Andrii Stiahar <andriy.styahar@gmail.com>
2025-10-25 17:41:19 +0300

    Add go/scala

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -33,6 +33,9 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.dap.core" },
     { import = "lazyvim.plugins.extras.formatting.prettier" },
     { import = "lazyvim.plugins.extras.lang.markdown" },
+    { import = "lazyvim.plugins.extras.lang.go" },
+    { import = "lazyvim.plugins.extras.lang.scala" },
+    { import = "lazyvim.plugins.extras.lang.terraform" },
     -- import/override with your plugins
     { import = "plugins" },
   },
